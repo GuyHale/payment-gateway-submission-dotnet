1a60bf156aeef0abd318e58ee035a30a2883e134
GuyHale <hale_guy@outlook.com>
2026-02-11 22:44:39 +0000

    remove validation from CurrencyCodesConfig

--- src/PaymentGateway.Api/Configuration/CurrencyCodesConfig.cs
+++ src/PaymentGateway.Api/Configuration/CurrencyCodesConfig.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace PaymentGateway.Api.Configuration;
+﻿namespace PaymentGateway.Api.Configuration;
 
 public class CurrencyCodesConfig
 {
     public const string SectionKey = "CurrencyCodesConfig";
-
-    [MinLength(1, ErrorMessage = "At least one currency code must be provided.")]
     public HashSet<string> CurrencyCodes { get; init; } = [];
 }
